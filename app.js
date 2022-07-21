7a20744d8cf156946bc817cb847bf3f0a68782d6
ASESIBO <cozzanibautista@gmail.com>
2022-07-21 17:39:24 -0300

    se cuadro el motor de tamplate

--- app.js
+++ app.js
@@ -6,32 +6,34 @@ const path=require('path');
 const rutaAbsoluta='./views/';
 app.use(express.static('public'));
 
-app.get('/', (req, res)=>{const htmlPath=path.resolve(__dirname,rutaAbsoluta+'home.html');
-    res.sendFile(htmlPath)
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=>{const htmlPath=path.resolve(__dirname,rutaAbsoluta+'home');
+    res.render(htmlPath)
 });
     
 app.get('/home', (req, res)=>{
-    const htmlPath=path.resolve(__dirname,rutaAbsoluta+'home.html');
-    res.sendFile(htmlPath)
+    const htmlPath=path.resolve(__dirname,rutaAbsoluta+'home');
+    res.render(htmlPath)
 });
 app.get('/registro', (req, res)=>{
-    const htmlPath=path.resolve(__dirname,rutaAbsoluta+'registro.html');
-    res.sendFile(htmlPath)
+    const htmlPath=path.resolve(__dirname,rutaAbsoluta+'registro');
+    res.render(htmlPath)
 });
 app.get('/carritodecompras', (req, res)=>{
-    const htmlPath=path.resolve(__dirname,rutaAbsoluta+'carrito.html');
-    res.sendFile(htmlPath)
+    const htmlPath=path.resolve(__dirname,rutaAbsoluta+'carrito');
+    res.render(htmlPath)
 });
 app.get('/login', (req, res)=>{
-    const htmlPath=path.resolve(__dirname,rutaAbsoluta+'login.html');
-    res.sendFile(htmlPath)
+    const htmlPath=path.resolve(__dirname,rutaAbsoluta+'login');
+    res.render(htmlPath)
 });
 app.get('/productos', (req, res)=>{
-    const htmlPath=path.resolve(__dirname,rutaAbsoluta+'productos.html');
-    res.sendFile(htmlPath)
+    const htmlPath=path.resolve(__dirname,rutaAbsoluta+'productos');
+    res.render(htmlPath)
 });
 app.get('/ayuda', (req, res)=>{
-    const htmlPath=path.resolve(__dirname,rutaAbsoluta+'ayuda.html');
-    res.sendFile(htmlPath)
+    const htmlPath=path.resolve(__dirname,rutaAbsoluta+'ayuda');
+    res.render(htmlPath)
 });
 app.listen(port, ()=>console.log("funcionando en localhost:"+port));
