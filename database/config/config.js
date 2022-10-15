c8a317c49560a756b184671470ac2ccd1bf0aa80
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2022-10-14 19:19:02 -0500

    Se mezcla

--- database/config/config.js
+++ database/config/config.js
@@ -1,8 +1,4 @@
 module.exports={
-<<<<<<< HEAD
-  
-=======
->>>>>>> e45f29121020a9e8b144f6eba117f33e7b12369f
   "development": {
     "username": "root",
     "password": "root",
@@ -24,9 +20,5 @@ module.exports={
     "database": "compras_gamer",
     "host": "127.0.0.1",
     "dialect": "mysql"
-<<<<<<< HEAD
-  
-=======
->>>>>>> e45f29121020a9e8b144f6eba117f33e7b12369f
   }
 }
