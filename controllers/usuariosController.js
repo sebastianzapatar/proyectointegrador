effdfb73e89a46612c91fd3f9c462332724811cd
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2022-11-02 20:04:22 -0500

    Se cuadra el login

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -18,7 +18,7 @@ const controller = {
 			user: req.session.userLogged
 		})
 	},
-	login: (req, res) => {
+	login2: (req, res) => {
 		let { email, password } = req.body;
 		const salt = bcrypt.genSaltSync(saltRounds);
 		//password = bcrypt.hashSync(password, salt);
@@ -28,6 +28,7 @@ const controller = {
 				email: email
 			}
 		}).then((user) => {
+			console.log(user);
 			const htmlPath = path.resolve(__dirname, rutaAbsoluta + 'login');
 			if (user != null) {
 				if (bcrypt.compareSync(password, user.password)) {
